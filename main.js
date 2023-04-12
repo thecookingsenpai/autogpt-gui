da9c51b8b360d8bad43b4dd423b845547f65b27e
Blockdrops LLC <67682496+thecookingsenpai@users.noreply.github.com>
2023-04-13 01:31:37 +0200

    Completed GUI pre-design & fixed layout errors

--- main.js
+++ main.js
@@ -14,16 +14,10 @@ function createWindow () {
     }
   })
 
-  // and load the index.html of the app.
+  // and loading the index.html of the app.
   mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
 
@@ -34,12 +28,7 @@ app.whenReady().then(() => {
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  app.quit()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
